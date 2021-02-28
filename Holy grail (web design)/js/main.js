9e2a3fd4ab52768525b06095a64402f30f8218a2
Brahim Amghough <bamghoug@e1r10p2.1337.ma>
2021-02-28 12:39:35 +0100

    added Holy grail (web design) project

---
+++ Holy grail (web design)/js/main.js
@@ -0,0 +1,13 @@
+var up = document.querySelector('#up');
+var down = document.querySelector('#down');
+var left = document.querySelector('#left');
+var right = document.querySelector('#right');
+
+var square = document.querySelector('.square');
+
+var index = 0;
+
+window.addEventListener('load', function(){
+    
+});
+window.onload +=
